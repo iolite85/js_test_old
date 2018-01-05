4863226d6332e853371a66f39bcef9b15184fdc0
유경민 <km.yoo1@cj.net>
2018-01-05 17:38:59 +0900

    vue.js 테스트

--- vue_js/backend/routes/test.js
+++ vue_js/backend/routes/test.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var tests = require('../user.json');
 
+
+
 router.get('/', function(req, res, next){
   res.send(tests)
 });
@@ -13,5 +15,36 @@ router.get('/:id', function(req, res, next){
   });
   res.send(tt)
 });
+
+router.post('/', function(req, res, next){
+  var fs = require('fs');
+  var fileData = req.body.user;
+
+  fs.readFile('user.json', function(err, obj) {
+    var fileObj = JSON.parse(obj);
+    fileObj.push(fileData);
+    //fileObj.push("id :"+fileData.id);
+    fs.writeFile('user.json', JSON.stringify(fileObj), function(err) {
+      if (err) {
+         res.status(500).jsonp({ error: 'Failed to write file' });
+      }
+      res.send("File write success");
+    });
+  });
+
+
+  /*fs.writeFile('user.json', JSON.stringify(fileData) , function(err) {
+    if (err) {
+       res.status(500).jsonp({ error: 'Failed to write file' });
+    }
+    res.send("File write success");
+  });*/
+
+  //console.log(fileData.id);
+  //res.send(req.body)
+
+});
+
+
 
 module.exports = router;
--- vue_js/frontend/src/components/UserAdd.vue
+++ vue_js/frontend/src/components/UserAdd.vue
@@ -14,15 +14,15 @@ export default {
       return {
         user: {
           id: '',
-          age: '',
-          name: ''
+          name: '',
+          age: ''
+
         }
       }
     },
 
     methods: {
       signUp: function (event) {
-        console.log(124);
         this.$http.post('http://localhost:3000/api/tests', { //axios 사용
           user: this.user
         })
@@ -34,7 +34,7 @@ export default {
           if (response.data.result === 1) {
             //alert('Success')
             console.log('Success');
-            //this.$router.push('/api/tests') // Login 페이지로 보내줌
+            this.$router.push('/api/tests') // Login 페이지로 보내줌
           }
         })
         .catch(function (error) {
