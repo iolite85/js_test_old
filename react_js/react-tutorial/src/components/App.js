a17a54c91f35129223dd3afb83080023401804c2
유경민 <kmyoo@hivelab.co.kr>
2018-01-26 18:00:43 +0900

    2.react JSX test

--- react_js/react-tutorial/src/components/App.js
+++ react_js/react-tutorial/src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
+  sayHey(){
+    alert("hey");
+  }
+
   render(){
+    let text = "Dev-Server";
+    let pStyle = {
+        color: 'aqua',
+        backgroundColor: 'black'
+    };
     return(
-      <h1>Hello React Skeleton</h1>
+      <div>
+      { /* 컴포넌트에서 여러 Element를 렌더링 해야 할 때 element들은 container element(하나의 element) 안에 포함시켜줘야함. 주석도 container element 안에 작성되어야 함. 단 주석은 브라우저상 source에 나타나지 않음   */ }
+        <h1>Hello React Skeleton</h1>
+        <h2>Welcome to {text}</h2>
+        <button onClick={this.sayHey}>Click Me</button>
+        <p style = {pStyle}>{1 == 1 ? 'True' : 'False' }</p>
+      </div>
     );
   }
 }
