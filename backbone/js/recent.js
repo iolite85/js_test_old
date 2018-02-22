b1edc79989b7037ec10b5fefc525a2ea58326bf8
유경민 <kmyoo@hivelab.co.kr>
2018-02-22 18:14:40 +0900

    test 2 / npm install -g http-server

--- backbone/js/recent.js
+++ backbone/js/recent.js
@@ -1,3 +1,18 @@
 (function ($) {
+  $.when(
+    $.ajax({
+        dataType: "json",
+        url: "data.json"
+    })
+  ).done(function(json){
+    //핸들바 템플릿을 가져온다.
+    var source = $("#recent-prd").html();
+    //핸들바 템플릿을 컴파일한다.
+    var template = Handlebars.compile(source);
+    //핸들바 템플릿에 데이터를 바인딩해서 HTML을 생성, HTML을 뿌려준다.
+    $(template(json)).appendTo(".lately_product");
+  });
+
+
 
 })(jQuery);
