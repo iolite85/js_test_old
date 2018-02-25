e31c8c0d06d23ade5a700fbf983518e2755c9aff
iolite85 <iolite20@gmail.com>
2018-02-26 07:03:34 +0900

    test

--- backbone/js/recent.js
+++ backbone/js/recent.js
@@ -1,33 +1,107 @@
 (function ($) {
-  /*$.when(
-    $.ajax({
-        dataType: "json",
-        url: "data.json"
-    })
-  ).done(function(json){
-    //핸들바 템플릿을 가져온다.
-    var source = $("#recent-prd").html();
-    //핸들바 템플릿을 컴파일한다.
-    var template = Handlebars.compile(source);
-    //핸들바 템플릿에 데이터를 바인딩해서 HTML을 생성, HTML을 뿌려준다.
-    $(template(json)).appendTo(".lately_product");
-  });*/
-  var history = Backbone.Model.extend({});
-  var historyItems = Backbone.Collection.extend({
-      model : history,
-      url:'data.json'
-  });
-  /*var itemList = new historyItems();
-  itemList.fetch();
-  console.log(itemList);*/
-  var itemList = Backbone.View.extend({
-    el: $('.lately_product'),
-    template: $("#recent-prd").html(),
-    render: function(){
-      var that = this;
-      var items = new historyItems();
-    }
-  });
+	
+	var history = Backbone.Model.extend();
+	var historyItems = Backbone.Collection.extend({
+		  model : history,
+		  url:'data.json'
+	});
+	var itemList = Backbone.View.extend({
+		el: $('.lately_product'),
+		template: Handlebars.compile($("#recent-prd-group").html()), 
+		initialize: function(){
+			var self = this;
+			this.collection = new historyItems();
+			//var groups = _.groupBy(this.collection.toJSON(), 'insertDate');
+			//console.log(groups);
+			this.collection.fetch({
+				success: function(){
+					console.log('json loaded');
+					self.render();
+				},
+				error: function(){
+					console.log('json load error');
+				}
+			});
+		},
+		getGroups : function(){
+			return _.groupBy(this.collection.toJSON(), 'insertDate');
+		},
+		getRender : function(){
+			var tpl = $("#recent-prd").html(); 
+			Handlebars.registerPartial("item", tpl);			
+		},
+		render: function(){
+			this.getRender();
+			var data = this.getGroups();
+			this.$el.append(this.template({groups: data})); 
+			//console.log(data);
+			//console.log(arr);
+			return this;
+		}
+	});
+	
+	var items = new itemList();
+	
+	
+	/* test
+	
+	$.when(
+		$.ajax({
+			dataType: "json",
+			url: "data.json"
+		})
+	).done(function(json){
+		//핸들바 템플릿을 가져온다.
+		var source = $("#recent-prd").html();
+		//핸들바 템플릿을 컴파일한다.
+		var template = Handlebars.compile(source);
+		//핸들바 템플릿에 데이터를 바인딩해서 HTML을 생성, HTML을 뿌려준다.
+		$(template(json)).appendTo(".lately_product");
+	});
 
+	// 부분템플릿
+	$.when(
+		$.ajax({
+			dataType: "json",
+			url: "data.json"
+		})
+	).done(function(json){
+		//핸들바 템플릿을 가져온다.
+		var source = $("#recent-prd").html();
+		//핸들바 템플릿을 컴파일한다.
+		var template = Handlebars.compile($("#recent-prd-group").html());
+		Handlebars.registerPartial("item", source);
+		//핸들바 템플릿에 데이터를 바인딩해서 HTML을 생성, HTML을 뿌려준다.
+		$(template(json)).appendTo(".lately_product");
+	});
+	
+
+	var history = Backbone.Model.extend();
+	var historyItems = Backbone.Collection.extend({
+		  model : history,
+		  url:'data.json'
+	});
+	var itemList = Backbone.View.extend({
+		el: $('.lately_product'),
+		template: Handlebars.compile($("#recent-prd").html()),
+		render: function() {
+			var data = { items: historys.models.map(function(x) { return x.toJSON(); }) };
+			this.$el.append(this.template(data));
+			return this;
+		}
+	});
+
+	var historys = new historyItems();
+	var items = new itemList({model: historys});
+	historys.fetch({
+		success: function(){
+			console.log('json loaded');
+			items.render();
+		},
+		error: function () {
+		  console.log('json load error');
+		}
+	});
+	*/
 
 })(jQuery);
